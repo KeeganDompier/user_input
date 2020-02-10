3c0972757acdedd445ec270907d39a143ab33f00
KeeganDompier <58610698+KeeganDompier@users.noreply.github.com>
2020-02-10 14:04:05 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+let Name = game.askForString("What is your name")
+
+let favorite_color = game.askForString("What is your favorite color?")
+
+let dog_or_cat = game.askForString("Dogs or cats?")
+
+game.splash("your name is: " + Name +" your favorite color is " + favorite_color +"and you perfer " + dog_or_cat) 
